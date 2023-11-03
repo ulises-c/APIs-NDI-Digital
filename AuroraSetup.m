a9a14a3689e26a7966904e5f5790b3a078ccee5f
Ulises <ez1ollie@yahoo.com>
2023-11-03 15:26:32 -0700

    Update AuroraSetup.m

--- AuroraSetup.m
+++ AuroraSetup.m
@@ -1,5 +1,6 @@
 % aurora_device = AuroraDriver('/dev/ttyUSB0');
-aurora_device = AuroraDriver('/dev/cu.usbserial-1320');
+% aurora_device = AuroraDriver('/dev/cu.usbserial-1320');
+aurora_device = AuroraDriver('/dev/tty.usbserial-1320');
 serial_present = instrfind;
 
 if(~isempty(serial_present)) 
@@ -11,6 +12,8 @@ if(~isempty(serial_present))
     aurora_device.enablePortHandleDynamicAll();
     aurora_device.startTracking();
     aurora_device.BEEP('1');
+    aurora_device.updateSensorDataAll();
+%     aurora_device.GET(sensor_status)
     aurora_device.stopTracking();
     delete(aurora_device);
     
